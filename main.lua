9da54b6afe3db686946d526fe48ec8922695567b
Michael Beeler <Michael.Beeler@l3harris.com>
2021-04-18 12:22:18 -0400

    triangle: draw triangle vertices
    add functions to draw the vertices
    add test code to verify draw points

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
+require 'triangle'
+
 function love.load()
+    -- love.window.setMode(0,0, {vsync=true})
     total_time = 0
+    screen_w = love.graphics.getWidth()
+    screen_h = love.graphics.getHeight()
+    triangle = Triangle()
+    triangle:init(screen_w, screen_h)
 end
 
 function love.update(dt)
@@ -7,5 +14,6 @@ function love.update(dt)
 end
 
 function love.draw()
-
+    -- love.graphics.circle("fill", 10, 10, 2)
+    triangle:controller()
 end
--- triangle.lua
+++ triangle.lua
@@ -1,7 +1,35 @@
 require "class"
+require 'point'
 
 Triangle = class(
   function(a, name)
     a.name = name
   end
 )
+
+function Triangle:init(screen_w, screen_h)
+  print("Initializing Triangle")
+  self.screen_width = screen_w
+  self.screen_height = screen_h
+  self.edge_offset = 20 -- pixels
+  print(self.screen_width)
+  print(self.screen_height)
+  self.points = {}
+  -- Create the verticies of the triangle
+  p1 = Point()
+  p2 = Point()
+  p3 = Point()
+  p1:init(self.screen_width/2, self.edge_offset)
+  p2:init(self.edge_offset, self.screen_height - self.edge_offset)
+  p3:init(self.screen_width - self.edge_offset, self.screen_height - self.edge_offset)
+end
+
+function Triangle:controller()
+  self:draw()
+end
+
+function Triangle:draw()
+  love.graphics.circle("fill", p1.x, p1.y, 2)
+  love.graphics.circle("fill", p2.x, p2.y, 2)
+  love.graphics.circle("fill", p3.x, p3.y, 2)
+end
